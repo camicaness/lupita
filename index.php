245d957070e08eb8a9dd926b7ab6aaf36f2f121b
camicaness <86695381+camicaness@users.noreply.github.com>
2023-11-02 20:46:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/responsive.css">
+    <title>Lupita Uruguay</title>
+</head>
+<body>
+    <?php
+        include "navbar.html"
+    ?>
+    <section id="home-hero">
+        <div>
+            <h1>Un Lupita,<br>miles de opciones</h1>
+            <p>Armá tu buzo como quieras y gozátelo</p>
+        </div>
+        <div>
+            <a href="lupitas.html" class="button primary">Creá tu Lupita</a>
+            <a href="" class="button secondary">Explorá el catálogo</a>
+        </div>
+    </section>
+    <section>
+        <a href="" class="home-sections">Creá tu Lupita</a>
+        <a href="" class="home-sections">Pantalones</a>
+        <a href="" class="home-sections">Remeras y vestidos</a>
+        <a href="" class="button tertiary">Mirá todo lo que hay</a>
+    </section>
+    <div class="como-comprar">
+        <h2>¿Cómo comprar?</h2>
+        <p>Mandame un DM ;)</p>
+        <p>Buscá la prenda que más te cope, contame por Instagram y lo arreglamos!</p>
+        <a href="" class="button tertiary">Mandame un mensaje</a>
+    </div>
+    <div class="footer">
+        <img src="resources/logo.svg" alt="Logo de lupita">
+        <a href="">
+            <img src="resources/icons/instagram.svg" alt="Instagram">
+            lupita.uruguay
+        </a>
+    </div>
+    <script src="js.js"></script>
+</body>
+</html>
---
+++ js.js
@@ -0,0 +1,6 @@
+function cerrarMenu(){
+    document.getElementById("menu-dropdown").style.transform = "translateY(-400px)";
+}
+function abrirMenu(){
+    document.getElementById("menu-dropdown").style.transform = "translateY(0px)";
+}
